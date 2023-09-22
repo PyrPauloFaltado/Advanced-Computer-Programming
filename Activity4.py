52eeaddd1f2949c0f361b7cd7b7a82358b022d28
PyrPauloFaltado <145738888+PyrPauloFaltado@users.noreply.github.com>
2023-09-22 12:28:56 +0800

    Create Activity4.py

---
+++ Activity4.py
@@ -0,0 +1,40 @@
+print("Hello Peeps!")
+print("This is from a line code")
+print("5")
+print("5.5")
+print("Hello,World")
+
+sum1 = 50 + 29
+print(sum1)
+
+
+a = 7
+product = 5 * a
+print(product)
+
+a = 2
+b = 11
+a = a * 2
+c = a * b
+b = b + (c * 3)
+print(b)
+
+number1 = int(input("Enter number1: "))
+number2 = int(input("Enter number2: "))
+sum = number1 + number2
+print(sum)
+
+Squared = 3**2
+Cubed = 3**3
+print(Squared)
+print(Cubed)
+
+Name = "Gly"
+print("Your name is " + Name)
+
+Apple_price = 20
+Orange_price = 20
+if Apple_price == Orange_price:
+    print("True")
+else:
+    print("False")
